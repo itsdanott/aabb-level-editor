20b7f2a0d4fa31b57befc51615fdbf3cfc2ec75b
o›tone <33697887+itsdanott@users.noreply.github.com>
2024-09-29 23:44:43 +0200

    added customizable grid colors, fade distance, alpha etc

--- shaders/grid.frag.glsl
+++ shaders/grid.frag.glsl
@@ -4,10 +4,10 @@ in vec3 FragPos;
 out vec4 FragColor;
 
 uniform vec3 viewPos;
-uniform float gridScale;
-
-const vec3 colorBlack = vec3(0.0);
-const vec3 colorWhite = vec3(1.0);
+uniform float gridAlpha;
+uniform float gridFadeDist;
+uniform vec3 checkerColor1;
+uniform vec3 checkerColor2;
 
 
 void main()
@@ -16,9 +16,10 @@ void main()
     float check = mod(floor(FragPos.x) + floor(FragPos.z), 2.0);
     
     // If check is 0, use white; if 1, use black
-    vec3 color = mix(colorWhite, colorBlack, check);
+    vec3 color = mix(checkerColor1, checkerColor2, check);
 
-    float distanceToCamera = length(viewPos - FragPos);
+    float distanceToCamera = length(viewPos.xz - FragPos.xz);
 
-    FragColor = vec4(color, mix(0.0, 1.0, 1.0 / distanceToCamera));
+    FragColor = vec4(color, (1.0 - (distanceToCamera / gridFadeDist)) * gridAlpha);
+    // FragColor = vec4(color, mix(0.0, 1.0, 1.0 - (distanceToCamera / 32.0)));
 }
